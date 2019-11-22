cbcf5eca5d336c6956d46909449c2477c0ac116c
Henry <henrytutu@hotmail.com>
2019-11-22 11:33:58 -0500

    completed Lab 10

---
+++ Lab 10/Challenge.js
@@ -0,0 +1,36 @@
+function addition(){
+  var num1;
+  var num2;
+  var sum;
+  num1 = Number(document.getElementById("value1").value);
+  num2 = Number(document.getElementById("value2").value);
+  sum = num1 + num2;
+  document.getElementById("output").innerHTML = sum;
+}
+function subtraction(){
+  var num1;
+  var num2;
+  var difference;
+  num1 = Number(document.getElementById("value1").value);
+  num2 = Number(document.getElementById("value2").value);
+  difference = num1 - num2;
+  document.getElementById("output").innerHTML = difference;
+}
+function muliplication(){
+  var num1;
+  var num2;
+  var product;
+  num1 = Number(document.getElementById("value1").value);
+  num2 = Number(document.getElementById("value2").value);
+  product = num1 * num2;
+  document.getElementById("output").innerHTML = product;
+}
+function division(){
+  var num1;
+  var num2;
+  var sum;
+  num1 = Number(document.getElementById("value1").value);
+  num2 = Number(document.getElementById("value2").value);
+  quotient = num1 / num2;
+  document.getElementById("output").innerHTML = quotient;
+}
